0aa4785a657813d59f5b91152044c15c8e64f711
yukiesuta <matsumoto.yukiho.0704@gmail.com>
2022-07-09 11:44:10 +0900

    CSSファイルを追加して問題と選択肢の見た目を整えた、正解不正解ボックスは未着手

--- src/resources/views/quizyBlade/quizy.blade.php
+++ src/resources/views/quizyBlade/quizy.blade.php
@@ -4,24 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href = "./kuizy.css">
+    <link rel="stylesheet" href = "{{asset('/css/style.css')}}">
     <title>quizy</title>
 </head>
 <body>
 
 <div class="container" >
     @foreach($questions as $question)
-        <!-- P.82参照　デフォルトのループ変数 -->
-        <h1 class = "question">{{$loop->iteration}}.この地名は何と読む?</h1>
-        {{-- 選択肢をルートパラメタが一致している適切なquestion_idが一致しているもののみforeachする --}}
-        @foreach($choices->where('question_id', $question->id) as $choice)
-            <div class="choice" id="correct" >
-                {{$choice->name}}
-            </div>
-        @endforeach
+        <div class="quiz">
+            <!-- P.82参照　デフォルトのループ変数 -->
+            <h1 class = "question">{{$loop->iteration}}.この地名は何と読む?</h1>
+            <img src="/img/{{$question->image}}">
+            {{-- 選択肢をルートパラメタが一致している適切なquestion_idが一致しているもののみforeachする --}}
+            @foreach($choices->where('question_id', $question->id) as $choice)
+                <li class="choice" id="correct" >
+                    {{$choice->name}}
+                </li>
+            @endforeach
             <div>
                 正解は{{ $choices->where('question_id', $question->id)->where('valid', true)->first()->name }}です
             </div>
+        </div>
     @endforeach
 </div> 
 
