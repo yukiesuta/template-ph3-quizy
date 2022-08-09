52e127730bdff6318c84435979660c9a5e782803
yukiesuta <matsumoto.yukiho.0704@gmail.com>
2022-08-09 13:20:07 +0900

    ログイン先のページのhomeを作った

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Dashboard</div> --}}
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,11 +13,21 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
-                </div>
+                    <table border="1">
+                        <tr>
+                          <td>東京</td>
+                          <td>編集</td>
+                          <td>削除</td>
+                        </tr>
+                        <tr>
+                          <td>佐藤</td>
+                          <td>編集</td>
+                          <td>削除</td>
+                        </tr>
+                      </table>
+                {{-- </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 </div>
 @endsection
